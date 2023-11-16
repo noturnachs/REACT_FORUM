49b3f579ec2af27f3159c0621ff0722d0ef2852d
Lius <142863069+noturnachs@users.noreply.github.com>
2023-11-16 11:26:47 +0800

    Update DashboardBody.jsx

--- src/components/DashboardBody.jsx
+++ src/components/DashboardBody.jsx
@@ -640,7 +640,7 @@ const DashboardBody = ({ selectedCategory }) => {
         <div className="flex flex-col justify-center items-center">
         {announcement && (
   <div className="flex justify-center items-center">
-    <button className="btn btn-error mb-5 w-full">
+    <button className="btn btn-warning mb-5 w-full">
       {announcement}
     </button>
   </div>
