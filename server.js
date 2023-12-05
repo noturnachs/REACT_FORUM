d5cd99d46b7524cc4187c0e968a97cc421e611ad
Lius <142863069+noturnachs@users.noreply.github.com>
2023-12-05 15:19:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const establishConnection = () => {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   });
 
   db.connect((err) => {
